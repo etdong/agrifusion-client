ff3e131812101df1244513349a1ed2034848e663
etdong <eric@donger.ca>
2025-06-09 11:22:22 +0800

    simple title screen

--- src/init_game.tsx
+++ src/init_game.tsx
@@ -1,11 +1,16 @@
 import makeKaplayCtx from "./kaplay_context"
+import initTitle from "./scenes/title"
 
 export default async function initGame() {
 	const k = makeKaplayCtx()
 
     k.setLayers(['bg', 'game', 'fg'], 'game')
+
+    // load font
+    k.loadFont('moot-jungle', './fonts/moot-jungle.ttf')
 	
     // initialize scenes
+    initTitle(k)
 
-	k.go('menu')
+	k.go('title')
 }
---
+++ src/utils/cam_utils.tsx
@@ -0,0 +1,36 @@
+import type { KAPLAYCtx, Vec2 } from "kaplay";
+
+/**
+ * Update camera zoom depending on screen size. Useful for mobile.
+ * @param k KAPLAY context
+ */
+export const updateCamZoom = (k: KAPLAYCtx) => {
+    if (k.width() < 1000 || k.height() < 800) {
+        k.setCamScale(1)
+    } else {
+        k.setCamScale(1.2)
+    }
+}
+
+/**
+ * Center camera on a position
+ * @param k KAPLAY context
+ * @param posVec2 Position to center camera on
+ */
+export const updateCamPos = (k: KAPLAYCtx, posVec2: Vec2) => {
+    k.tween(
+        k.getCamPos(),
+        posVec2,
+        0.1,
+        (newPos) => k.setCamPos(newPos),
+        k.easings.linear
+    )
+}
+
+/**
+ * @param k KAPLAY context
+ * @returns The relative mouse position from the center of the screen
+ */
+export const getRelativeMousePos = (k: KAPLAYCtx) => {
+    return k.vec2(k.getCamPos().add((k.mousePos().x - k.center().x) / k.getCamScale().x, (k.mousePos().y - k.center().y) / k.getCamScale().y))
+}
